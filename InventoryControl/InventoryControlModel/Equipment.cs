0e9fee0ac423bb0286c29b1ec53cee17ea0f0bba
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-03-19 00:24:20 -0300

    Gera ArgumentException tratadas para datas do futuro
    -Equipment.ManufacturingDate e MaintenanceCall.OpeningDate agora não pode ser datas que ainda não chegaram (datas do futuro)

--- InventoryControl/InventoryControlModel/Equipment.cs
+++ InventoryControl/InventoryControlModel/Equipment.cs
@@ -31,7 +31,16 @@ namespace InventoryControlModel
         }
         public float AcquisitionPrice { get => acquisitionPrice; set => acquisitionPrice = value; }
         public string SerialNumber { get => serialNumber; set => serialNumber = value; }
-        public DateTime ManufacturingDate { get => manufacturingDate; set => manufacturingDate = value; }
+        public DateTime ManufacturingDate
+        {
+            get { return manufacturingDate; }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("ManufacturingDate property cannot be set as a date from the future.");
+                manufacturingDate = value;
+            }
+        }
         public string ManufacturerName { get => manufacturerName; set => manufacturerName = value; }
     }
 }
